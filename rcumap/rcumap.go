84170b488858bb7132104ac583a15c800f197275
perhydrol <50535400+perhydrol@users.noreply.github.com>
2025-12-30 15:33:37 +0800

    refactor(rcumap): use sharding to improve write concurrency
    - Shard the map using maphash to distribute keys and enable parallel writes across independent segments
    - Replace global lock with per-shard mutexes to reduce contention under high concurrent write load
    - Add atomic Size() method to track element count efficiently without locking the entire map

--- rcumap/rcumap.go
+++ rcumap/rcumap.go
@@ -1,6 +1,7 @@
 package rcumap
 
 import (
+	"hash/maphash"
 	"maps"
 	"sync"
 	"sync/atomic"
@@ -10,47 +11,73 @@ type RCUMap[K comparable, V any] interface {
 	Get(K) (V, bool)
 	Set(K, V)
 	Del(K)
+	Size() int
 }
 
 type rcuMap[K comparable, V any] struct {
-	holder atomic.Pointer[map[K]V]
+	// 使用指针可以有效避免slice扩容时进行大量拷贝，并且sync.Mutex本身也不允许拷贝操作
+	shards []*rcuShard[K, V]
+	len    int32
+	cap    int
+	h      maphash.Seed
+}
 
-	mu sync.Mutex
+type rcuShard[K comparable, V any] struct {
+	holder atomic.Pointer[map[K]V]
+	mu     sync.Mutex
 }
 
 func NewRCUMap[K comparable, V any]() RCUMap[K, V] {
-	m := make(map[K]V)
-	r := &rcuMap[K, V]{}
-	r.holder.Store(&m)
-	return r
+	cap := 10
+	shards := make([]*rcuShard[K, V], 0, 10)
+	for range cap {
+		m := map[K]V{}
+		r := rcuShard[K, V]{}
+		r.holder.Store(&m)
+		shards = append(shards, &r)
+	}
+	r := rcuMap[K, V]{shards: shards, len: 0, cap: cap, h: maphash.MakeSeed()}
+	return &r
 }
 
-func copyMap[K comparable, V any](m map[K]V) map[K]V {
-	newMap := make(map[K]V, len(m)+1)
-	maps.Copy(newMap, m)
-	return newMap
+func (m *rcuMap[K, V]) hash(key K) uint {
+	return uint(maphash.Comparable(m.h, key))
+}
+
+func (m *rcuMap[K, V]) Size() int {
+	return int(atomic.LoadInt32(&m.len))
 }
 
 func (m *rcuMap[K, V]) Set(key K, value V) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	oldMap := *m.holder.Load()
-	newMap := copyMap(oldMap)
+	shardIndex := m.hash(key) % uint(m.cap)
+	shard := m.shards[shardIndex]
+	shard.mu.Lock()
+	defer shard.mu.Unlock()
+	oldMap := *shard.holder.Load()
+	newMap := make(map[K]V, len(oldMap))
+	maps.Copy(newMap, oldMap)
 	newMap[key] = value
-	m.holder.Store(&newMap)
+	shard.holder.Store(&newMap)
+	atomic.AddInt32(&m.len, 1)
 }
 
 func (m *rcuMap[K, V]) Get(key K) (V, bool) {
-	h := *m.holder.Load()
-	val, ok := h[key]
-	return val, ok
+	shardIndex := m.hash(key) % uint(m.cap)
+	shard := m.shards[shardIndex]
+	holder := *shard.holder.Load()
+	v, ok := holder[key]
+	return v, ok
 }
 
 func (m *rcuMap[K, V]) Del(key K) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	oldMap := *m.holder.Load()
-	newMap := copyMap(oldMap)
+	shardIndex := m.hash(key) % uint(m.cap)
+	shard := m.shards[shardIndex]
+	shard.mu.Lock()
+	defer shard.mu.Unlock()
+	oldMap := *shard.holder.Load()
+	newMap := make(map[K]V, len(oldMap))
+	maps.Copy(newMap, oldMap)
 	delete(newMap, key)
-	m.holder.Store(&newMap)
+	shard.holder.Store(&newMap)
+	atomic.AddInt32(&m.len, -1)
 }
